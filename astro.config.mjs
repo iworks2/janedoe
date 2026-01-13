0452bc23ee644d8feb4b3ce334121b73f0ccb835
Marion D. Williams <marion@rtbp.net>
2026-01-12 19:49:40 -0600

    Add tailwind dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import keystatic from '@keystatic/astro';
 import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [mdx(), keystatic(), react()],
+  integrations: [mdx(), keystatic(), react(), tailwind()],
   output: 'server'
 });
