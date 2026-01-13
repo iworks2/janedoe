b33e444e2bdad109e423cc6901e2b8ad13fc0b3f
Marion D. Williams <marion@rtbp.net>
2026-01-12 19:53:27 -0600

    Add Azure node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import mdx from '@astrojs/mdx';
 import keystatic from '@keystatic/astro';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import node from '@astrojs/node';
 
 export default defineConfig({
   integrations: [mdx(), keystatic(), react(), tailwind()],
-  output: 'server'
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
